cf038ec5871af8a291d5c5b94ecadd7939696eab
kaysmink <kaysmink@live.nl>
2022-12-03 11:10:28 +0100

    added Day 3 onliners

--- src/Day 3. Rucksack Reorganization.py
+++ src/Day 3. Rucksack Reorganization.py
@@ -9,3 +9,8 @@ print(sum([ord(char) - 96 if char.islower() else ord(char)-38 for char in inters
 groups = [Input[i:i+3] for i in range(0,len(Input), 3)]
 intersections = [list(set(elf1).intersection(set(elf2), set(elf3)))[0] for elf1, elf2, elf3 in groups]
 print(sum([ord(char) - 96 if char.islower() else ord(char)-38 for char in intersections]))
+
+
+# oneline monsters
+print(sum([ord(char) - 96 if char.islower() else ord(char)-38 for char in [list(set(com1).intersection(set(com2)))[0] for com1, com2 in [[list(line[0:len(line)//2]), list(line[len(line)//2:])] for line in Input]]]))
+print(sum([ord(char) - 96 if char.islower() else ord(char)-38 for char in [list(set(elf1).intersection(set(elf2), set(elf3)))[0] for elf1, elf2, elf3 in [Input[i:i+3] for i in range(0,len(Input), 3)]]]))
