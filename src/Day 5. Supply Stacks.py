2145265fdc3e10eb4f9a3b1c6361f82f70f68d03
kaysmink <kaysmink@live.nl>
2022-12-05 10:49:48 +0100

    Added Day 5

---
+++ src/Day 5. Supply Stacks.py
@@ -0,0 +1,26 @@
+import re 
+
+Input = open("data/dag 5. input.txt", "r").read().split("\n\n")[1].split("\n")
+procedures = [list(map(int,re.findall(r'\d+', line))) for line in Input][:-1]
+
+# handmatig de start stacks in list gezet. Aangezien parsen meer tijd kost
+stacks = [list(string) for string in ["HCR", "BJHLSF", "RMDHJTQ", "SGRHZBJ", "RPFZTDCB", "THCG", "SNVZBPWL", "RJQGC", "LDTRHPFS"]]
+
+# Deel 1 
+for n, fro, to in procedures:
+    for step in range(n):
+        stacks[to-1].append(stacks[fro-1].pop())
+        
+print("".join([stack[-1] for stack in stacks]))
+
+# Deel 2
+stacks = [list(string) for string in ["HCR", "BJHLSF", "RMDHJTQ", "SGRHZBJ", "RPFZTDCB", "THCG", "SNVZBPWL", "RJQGC", "LDTRHPFS"]]
+
+for n, fro, to in procedures:
+    popped = [] 
+    for step in range(n):
+        popped.append(stacks[fro-1].pop())
+    popped = list("".join(popped)[::-1])
+    stacks[to-1].extend(popped)
+        
+print("".join([stack[-1] for stack in stacks]))
